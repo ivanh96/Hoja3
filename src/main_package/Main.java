507accb7f9bc50ebb00be19d11c377f58fe60234
Fredy Espana 15060 <fredy.espc@gmail.com>
2016-07-28 23:01:16 -0600

    Random Gen V1.0
    Generador de 3000 números random en un archivo .txt
    Para la siguiente versión revisará si ya existe el archivo con ese nombre.

---
+++ src/main_package/Main.java
@@ -0,0 +1,11 @@
+package main_package;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Random_Gen rnd = new Random_Gen();
+		rnd.RandomFill();
+
+	}
+
+}
---
+++ src/main_package/Random_Gen.java
@@ -0,0 +1,33 @@
+package main_package;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class Random_Gen {
+	/* Atributos */
+	Random rand = new Random();
+	Integer[] rnd_list = new Integer[3000];
+	
+
+	public void RandomFill(){
+	/* Se generan 3000 numeros pseudo random */
+		int rnd_value;
+		for(int i=0; i<3000; i++){
+			rnd_value = rand.nextInt(3000)+1;
+			rnd_list[i] = rnd_value;
+		}
+	/* Fill de numeros random en el .txt */	
+	try{
+		FileWriter writer = new FileWriter("Random_DB.txt", true);
+		for(int c=0; c<rnd_list.length; c++){
+			writer.write(rnd_list[c] + " ");
+		}
+		writer.close();
+	}
+	catch(IOException e){
+		System.out.println("Ups! No ha sido posible guardar los numeros.");
+	}
+	}
+	
+
+}
