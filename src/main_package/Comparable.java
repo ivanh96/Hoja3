75bc8d77c39392a3cbbee0734d333c6b776a60a5
ivanh96 <ivanh96@hotmail.com>
2016-07-29 19:03:41 -0600

    Add files via upload

---
+++ src/main_package/Comparable.java
@@ -0,0 +1,60 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public interface Comparable {
+
+    /**
+     * 
+     */
+    public Comparable() {
+    }
+
+    /**
+     * @param List[int] 
+     * @return
+     */
+    public void selectionSort(lista List[int]) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param List[int] 
+     * @return
+     */
+    public void insertionSort(lista List[int]) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param List[int] 
+     * @return
+     */
+    public void mergeSort(lista List[int]) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param List[int] 
+     * @return
+     */
+    public void quickSort(lista List[int]) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param List[int] 
+     * @return
+     */
+    public void radixSort(lista List[int]) {
+        // TODO implement here
+        return null;
+    }
+
+}
---
+++ src/main_package/Sort.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Sort {
+
+    /**
+     * 
+     */
+    public Sort() {
+    }
+
+
+    /**
+     * 
+     */
+    public void Sort() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public List[int] listRead() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void listSave() {
+        // TODO implement here
+        return null;
+    }
+
+}
