7b9ee0ded1b4569d1bf1beee025245f1b4570d04
Fredy Espana 15060 <fredy.espc@gmail.com>
2016-07-29 22:04:49 -0600

    Add files via upload
    Corregido

--- src/main_package/Sort.java
+++ src/main_package/Sort.java
@@ -1,5 +1,23 @@
 package main_package;
 
 public class Sort {
-
+	
+	public void selectionSort(Integer[] list){
+		int minNumber = 0;
+		
+		for (int i=0; i<list.length; i++){
+			minNumber = i;
+			for(int c=(i+1); c < list.length; c++){
+				if(list[c] < list[minNumber]){
+					minNumber = c;
+				}
+			}
+			if (minNumber != i){
+				int temp_position = list[i];
+				list[i] = list[minNumber];
+				list[minNumber] = temp_position;
+			}
+			System.out.println(list[i]);
+		}
+		}
 }
