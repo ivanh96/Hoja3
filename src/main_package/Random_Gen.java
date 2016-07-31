fffb7c62d3ba9e90a1dd9e760d23750166999e05
diego1soler <dsoler1998@gmail.com>
2016-07-30 22:05:34 -0600

    Read Integer From Text File And Save Them In Array

--- src/main_package/Random_Gen.java
+++ src/main_package/Random_Gen.java
@@ -1,13 +1,21 @@
 package main_package;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
+import java.util.Scanner;
+
+
 
 public class Random_Gen {
 	/* Atributos */
 	Random rand = new Random();
 	Integer[] rnd_list = new Integer[3000];
-	
+	Integer[] rnd_list2 = new Integer[3000];
+
 
 	public Integer[] RandomFill(){
 	/* Se generan 3000 numeros pseudo random */
@@ -16,6 +24,7 @@ public class Random_Gen {
 			rnd_value = rand.nextInt(10000)+1;
 			rnd_list[i] = rnd_value;
 		}
+		
 	/* Fill de numeros random en el .txt */	
 	try{
 		FileWriter writer = new FileWriter("Random_DB.txt", true);
@@ -28,6 +37,47 @@ public class Random_Gen {
 		System.out.println("Ups! No ha sido posible guardar los numeros.");
 	}
 	return rnd_list;
+	}
+	
+
+	
+// LEER LOS NUMEROS DESDE EL ARCHIVO DE TEXTO
+	public Integer[] Read(String direccion){
+		
+		int i = 0;
+		File file = new File(direccion);
+	    Scanner inputFile = null;
+	     try {
+	        inputFile = new Scanner(file);
+	     } 
+	
+	        catch (FileNotFoundException Exception) {
+	           System.out.println("No hay archivo amigo");
+	        }
+	   
+	     if (inputFile != null) {
+	    	  
+	    	  try {
+	    	    while (inputFile.hasNext()) {
+	    	      if (inputFile.hasNextInt() && i <3000) {
+	    	        rnd_list2[i] = inputFile.nextInt();
+	    	        i++;
+	    	      } else {
+	    	        inputFile.next();
+	    	      }
+	    	    }
+	    	  } finally {
+	    	    inputFile.close();
+	    	  }
+	    	 
+	    	}
+				
+				
+		return rnd_list2;		
+			
+				
+
+				
 	}
 	
 	
